c06509e4ac56877f6aedd9a4760ebe4ae5bcf534
Nicholas Ochs <nick.ochs@yahoo.com>
2020-08-07 10:02:46 -0400

    fixed image changer

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,9 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/athena.jpeg') {
-        myImage.setAttribute('src','images/athena2.jpeg');
+    if(mySrc === 'images/athena.jpg') {
+        myImage.setAttribute('src','images/athena2.jpg');
     } else {
-        myImage.setAttribute('src','images/athena.jpeg');
+        myImage.setAttribute('src','images/athena.jpg');
     }
 }

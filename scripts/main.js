015bdfe39b0f3454f6b10048fe35bde92d8056a0
Nicholas Ochs <nick.ochs@yahoo.com>
2020-08-17 21:18:11 -0400

    Added personalized greeting message

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,26 @@ myImage.onclick = function() {
         myImage.setAttribute('src','images/athena.jpg');
     }
 }
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+        setUserName();
+    } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = myName + ', meet Athena';
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = storedName + ', meet Athena';
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
